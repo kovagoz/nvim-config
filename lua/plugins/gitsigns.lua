6a74c2afd1adfed0eeeb52ed5be4a6ce445348ed
Zoltan Kovago <zoltan.kovago@hu.ibm.com>
2025-10-03 14:11:17 +0200

    feat(gitsigns): Add key binding to reset hunk

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,5 +5,6 @@ return {
     { "]h", "<cmd>Gitsigns next_hunk<CR>", desc = "Next hunk" },
     { "[h", "<cmd>Gitsigns prev_hunk<CR>", desc = "Previous hunk" },
     { "<leader>gb", "<cmd>Gitsigns blame<CR>", desc = "Git blame" },
+    { "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", desc = "Reset hunk" },
   }
 }
