2693fd814a19dc729836452ad4328df8c62aec2a
Zoltan Kovago <zoltan.kovago@hu.ibm.com>
2025-10-09 11:16:38 +0200

    del(nvim-lspconfig): Custom config for `helm_ls`

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,13 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   config = function ()
---    vim.lsp.config["helm_ls"] = {
---      filetypes = { "helm", "helm.yaml" },
---      root_markers = { "Chart.yaml" },
---      get_language_id = function ()
---        return "helm"
---      end
---    }
     vim.lsp.enable("helm_ls")
   end
 }
