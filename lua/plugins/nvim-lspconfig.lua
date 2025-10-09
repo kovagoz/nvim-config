3cf7c1af2d0050b767a0b4beeb4bf3d27e6269ae
Zoltan Kovago <zoltan.kovago@hu.ibm.com>
2025-10-09 11:19:27 +0200

    feat(nvim-lspconfig): Configure for Dockerfile

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,22 @@
 return {
   "neovim/nvim-lspconfig",
   config = function ()
+    local lspconfig = require('lspconfig')
+    local configs = require('lspconfig.configs')
+
+    if not configs.dockerfile_language_server then
+      configs.dockerfile_language_server = {
+        default_config = {
+          cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/docker-langserver'), '--stdio' },
+          filetypes = { 'dockerfile' },
+          root_dir = lspconfig.util.root_pattern('.git'),
+          single_file_support = true,
+        },
+      }
+    end
+
+    lspconfig.dockerfile_language_server.setup({})
+
     vim.lsp.enable("helm_ls")
     vim.lsp.enable("intelephense")
   end
